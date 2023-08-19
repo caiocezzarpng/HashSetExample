a72c72a430971becc59f848a63d287a07ada456a
caiocezzar <caiocezzarpng@gmail.com>
2023-08-19 17:07:42 -0300

    Add project files.

---
+++ HashSetExample/Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetExample.Entities
+{
+    internal class LogRecord
+    {
+        public string User { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return User.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord)) return false;
+
+            LogRecord other = obj as LogRecord;
+            return User.Equals(other.User);
+        }
+    }
+}
---
+++ HashSetExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using HashSetExample.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HashSetExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+
+                        set.Add(new LogRecord { User =  name, Instant = instant });
+                    }
+
+                    Console.WriteLine($"Total Users: {set.Count} ");
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
